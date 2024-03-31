08925aa852b8260b64594c52978680d8a5120706
John Doe <johndoe@email.com>
2024-03-30 23:23:13 -0500

    updates

--- main.py
+++ main.py
@@ -4,11 +4,14 @@ import pytz
 from discord.ext import commands, tasks
 import discord
 from secret import Secret
+import asyncio
+from datetime import datetime, timedelta
 
 intents = discord.Intents.default()
 intents.messages = True
 bot = commands.Bot(command_prefix='!', intents=intents)
 use_historical_data = True  # Set to False for live data
+
 
 # Schedule the task and start the bot
 @bot.event
@@ -20,7 +23,7 @@ async def on_ready():
 
 def fetch_ohlc_data(symbol, start_date, end_date, interval, api_key):
     # Correcting interval and timespan
-    timespan = "minute"  # For minute data
+    timespan = "day"  # For minute data
     multiplier = "1"  # For 1-minute intervals
     url = f"https://api.polygon.io/v2/aggs/ticker/{symbol}/range/{multiplier}/{timespan}/{start_date}/{end_date}?adjusted=true&sort=asc&apiKey={api_key}"
     response = requests.get(url)
@@ -32,15 +35,20 @@ def fetch_ohlc_data(symbol, start_date, end_date, interval, api_key):
 
 
 
-@tasks.loop(seconds=20 if use_historical_data else 60)
+@tasks.loop(count=1)
 async def check_and_alert():
     api_key = "RG34KJaw5GqpozaHArfsZ7I2P5kAVlmG"
-    symbols = ["AAPL"]  # Your symbols of interest
+    symbols = ["SPY", "QQQ", "AAPL", "MSFT", "GOOGL", "AMZN", "META", "AMD", "ZM", "WMT",
+       "JPM", "SPOT", "RBLX", "RDDT", "DIS", "NVDA", "ABNB", "PYPL", "SNAP", "IWM",
+       "ADBE", "NFLX", "HOOD"
+       ]
+    # symbols = ["RIVN"
+    #     ]
     intervals = ["1"]  # Representing the intervals as needed by Polygon.io
     
     if use_historical_data:
-        start_date = "2024-03-01"  # Example start date for historical data
-        end_date = "2024-03-03"  # Example end date for historical data
+        start_date = "2024-03-27"  # Example start date for historical data
+        end_date = "2024-03-29"  # Example end date for historical data
     else:
         now = datetime.now(pytz.timezone('US/Eastern'))
         start_date = end_date = now.strftime('%Y-%m-%d')
@@ -52,58 +60,109 @@ async def check_and_alert():
             # Fetch OHLC data for the current interval
             ohlc_data = fetch_ohlc_data(symbol, start_date, end_date, interval_value, api_key)
             if ohlc_data:
+
                 if use_historical_data:
                     # Iterate through each data point for historical testing
-                    for i in range(1, len(ohlc_data)):
-                        process_data_point([ohlc_data[i], ohlc_data[i-1]], symbol)
+                    for i in range(0, len(ohlc_data) - 1):
+                        await process_data_point(ohlc_data[i], ohlc_data[i+1], symbol)
 
                 else:
                     # For live data, process only the latest data point
-                    process_data_point([ohlc_data[-1], ohlc_data[-2]], symbol)
+                    await process_data_point([ohlc_data[0], ohlc_data[1]], symbol)
+            await asyncio.sleep(3)
+    
+    print(f"No more symbols.")
 
-async def process_data_point(data_point, symbol):
-    # Analyze the data point
-    analysis_result = analyze_for_reversal([data_point])
+async def process_data_point(data_point_1, data_point_2, symbol):
+    print(f"Analyzing {symbol}...")
+    await asyncio.sleep(2)
+    print(f"Testing {symbol} short...")
+    await asyncio.sleep(2)
+    analysis_result = analyze_for_shorts(data_point_1, data_point_2, symbol)  # Assuming analyze_for_reversal expects a list of OHLC data points
     if analysis_result:
-        timestamp = datetime.fromtimestamp(data_point['t'] / 1000).strftime('%Y-%m-%d %H:%M:%S')
-        message = f"[{timestamp}] {symbol} signal: {analysis_result['signal']} at {analysis_result['entry_point']}, Stop-loss: {analysis_result['stop_loss']}"
+        timestamp = (datetime.fromtimestamp(data_point_2['t'] / 1000) + timedelta(days=1)).strftime('%m-%d-%Y')
+        message = f"SHORT Alert: {symbol}, Entry: {analysis_result['entry_point']}, Stop: {analysis_result['stop_loss']} | {timestamp}"
+        print(message)
         await bot.get_channel(Secret.signal_channel_id).send(message)
+        await asyncio.sleep(2)
+    else:
+        print(f"No short detected.")
+        await asyncio.sleep(2)
 
-def analyze_for_reversal(ohlc_data):
-    """
-    Analyze OHLC data to find reversal candlestick patterns.
-    
-    :param ohlc_data: List of dictionaries, each containing OHLC data for a single candlestick
-    :return: Dictionary with signal information, or None if no pattern is detected
-    """
-    print(f"Inside analyze_for_reversal")
-    if len(ohlc_data) < 2:
-        print(f"Not enough data: {len(ohlc_data)}")
-        return None  # Not enough data to identify the pattern
-    
-    # Assuming ohlc_data is sorted in ascending order by time
-    last_candle = ohlc_data[-1]
-    prev_candle = ohlc_data[-2]
-    
+    print(f"Testing {symbol} long...")
+    await asyncio.sleep(2)
+    analysis_result_long = analyze_for_longs(data_point_1, data_point_2, symbol)  # Assuming analyze_for_reversal expects a list of OHLC data points
+    if analysis_result_long:
+        timestamp = (datetime.fromtimestamp(data_point_2['t'] / 1000) + timedelta(days=1)).strftime('%m-%d-%Y')
+        message = f"LONG Alert: {symbol}, Entry: {analysis_result_long['entry_point']}, Stop: MANAGE YOUR TRADE | {timestamp}"
+        print(message)
+        await bot.get_channel(Secret.signal_channel_id).send(message)
+        await asyncio.sleep(2)
+    else:
+        print(f"No long detected.")
+        await asyncio.sleep(2)
 
-    print(f"Checking data: {last_candle}")
-    # Determine if the last candle is a sender candle
+def analyze_for_shorts(data_point_1, data_point_2, symbol):
     is_sender = False
-    if last_candle['h'] > prev_candle['h'] and (last_candle['c'] < last_candle['o'] if last_candle['c'] > prev_candle['c'] else last_candle['c'] <= last_candle['o']):
-        is_sender = True
-
-    print(f"Checking data: {last_candle['c']}")
-
+    recent_candle = data_point_2
+    prev_candle = data_point_1
+    
+    # Determine if the last candle is a sender candle
+    
+    if recent_candle['h'] > prev_candle['h']:
+        #green candle first
+        #red candle secondary
+        if recent_candle['c'] < recent_candle['o']:
+            if recent_candle['c'] < prev_candle['c']:
+                entry_point = recent_candle['o']
+                is_sender = True
+            if recent_candle['l'] < prev_candle['l']:
+                is_sender = False
+        #green candle secondary
+        if recent_candle['c'] > recent_candle['o']:
+            if recent_candle['c'] < prev_candle['c']:
+                entry_point = recent_candle['c']
+                is_sender = True
+            if recent_candle['l'] < prev_candle['l']:
+                is_sender = False
     if is_sender:
-        # Determine the entry point based on the color of the sender candle
-        entry_point = last_candle['c'] if last_candle['c'] < last_candle['o'] else last_candle['o']
         return {
-            'ticker': 'Symbol_Name',  # Replace with actual symbol
+            'ticker': symbol,
             'entry_point': entry_point,
-            'signal': 'SELL' if last_candle['c'] > last_candle['o'] else 'BUY',  # Assuming a reversal to downtrend
-            'stop_loss': last_candle['h']  # Add this line
+            'stop_loss': recent_candle['h']
         }
+    return None
 
+
+def analyze_for_longs(data_point_1, data_point_2, symbol):
+    is_sender = False
+    recent_candle = data_point_2
+    prev_candle = data_point_1
+    
+    # Determine if the last candle is a sender candle
+    
+    if recent_candle['l'] < prev_candle['l']:
+        #red candle first
+        #green candle secondary
+        if recent_candle['o'] < recent_candle['c']:
+            if recent_candle['c'] < prev_candle['o']:
+                entry_point = prev_candle['h']
+                is_sender = True
+            if recent_candle['h'] > prev_candle['h']:
+                is_sender = False
+        #red candle secondary
+        if recent_candle['o'] > recent_candle['c']:
+            if recent_candle['c'] > prev_candle['c']:
+                entry_point = prev_candle['h']
+                is_sender = True
+            if recent_candle['h'] > prev_candle['h']:
+                is_sender = False
+    if is_sender:
+        return {
+            'ticker': symbol,
+            'entry_point': entry_point,
+            'stop_loss': recent_candle['l']
+        }
     return None
 
    
